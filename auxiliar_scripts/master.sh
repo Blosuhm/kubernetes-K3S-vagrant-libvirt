7294af19a77701c5d00cd6d9645c863231f2c085
fungame2270 <tomasvictal@gmail.com>
2025-07-09 16:13:25 +0100

    changed file location of auxiliar scripts

--- auxiliar_scripts/master.sh
+++ auxiliar_scripts/master.sh
@@ -9,4 +9,4 @@ echo "[TASK 2] Generate and save cluster join command to /vagrant/joincluster.sh
 K3S_NODE_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 
 JOIN_COMMAND="curl -sfL https://get.k3s.io | K3S_URL=https://master:6443 K3S_TOKEN=${K3S_NODE_TOKEN} sh -"
-echo "$JOIN_COMMAND" > /vagrant/joincluster.sh
+echo "$JOIN_COMMAND" > /vagrant/auxiliar_scripts/joincluster.sh
---
+++ auxiliar_scripts/worker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "[INFO] Waiting for joincluster.sh to become available..."
+while [ ! -f /vagrant/auxiliar_scripts/joincluster.sh ]; do
+  sleep 2
+done
+
+# Join worker nodes to the Kubernetes cluster
+echo "[TASK 1] Join node to Kubernetes Cluster"
+bash /vagrant/auxiliar_scripts/joincluster.sh
